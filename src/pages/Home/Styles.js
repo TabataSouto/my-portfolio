eb7eea817a572d853fbd9ef3888edbd8542176e5
Tabata <tabatac.souto@gmail.com>
2022-06-11 22:29:47 -0300

    feat: criação da página home.

---
+++ src/pages/Home/Styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  color: white;
+`
+
+const styles = {
+  Container,
+}
+
+export default styles;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import Styles from './Styles';
+
+// import Header from '../../components/Header';
+// import Footer from '../../components/Footer';
+
+function Home() {
+  return (
+    <Styles.Container>
+      {/* <Header />
+      <Footer /> */}
+    </Styles.Container>
+  )
+}
+
+export default Home;
