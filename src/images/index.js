a99b8075c9c94dabf01d4618ebb572f990952050
Tabata <tabatac.souto@gmail.com>
2022-06-14 10:42:03 -0300

    refactor: organização pasta de imagens e imortações.

---
+++ src/images/index.js
@@ -0,0 +1,21 @@
+import iconBaterry from './baterry.png';
+import gameController from './controller-game-100.png';
+import iconGamerOne from './gamer-1.png';
+import iconGamerTwo from './gamer-2.png';
+import iconHand from './hand-icon.png';
+import iconHome from './icon-home.png';
+import tabata from './tabata.jpeg';
+import wiFi from './wi-fi.png';
+
+const icons = {
+  iconBaterry,
+  gameController,
+  iconGamerOne,
+  iconGamerTwo,
+  iconHand,
+  iconHome,
+  tabata,
+  wiFi
+}
+
+export default icons;
