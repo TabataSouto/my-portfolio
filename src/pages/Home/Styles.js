82ca68afff652efd58e673d50a729b747c376a2a
Tabata <tabatac.souto@gmail.com>
2022-06-15 12:51:49 -0300

    fix(style): correção da estilização da página Home.

--- src/pages/Home/Styles.js
+++ src/pages/Home/Styles.js
@@ -4,9 +4,8 @@ const Main = styled.main`
   display: flex;
   flex-direction: column;
   height: 100vh;
-  justify-content: space-between;
+  justify-content: space-evenly;
 `
-
 
 const Styles = {
   Main,
