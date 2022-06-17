5d31b8793d5096f4513a767885fa908e17fc72c2
Tabata <tabatac.souto@gmail.com>
2022-06-17 14:58:43 -0300

    feat: criação da página Contatos.

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,28 @@
+import Styles from './Styles'
+import icons from '../../images';
+
+import MainHeader from '../../components/MainHeader';
+import Footer from '../../components/Footer';
+
+function Contact() {
+  return (
+    <Styles.Main>
+      <MainHeader
+        image={ icons.iconContact }
+        text="Contato"
+      />
+      <Footer
+        buttonX='Home'
+        pathButtonX='/home'
+        buttonA='Projetos'
+        pathButtonA='/projects'
+        buttonY='Soft Skills'
+        pathButtonY='/soft-skills'
+        buttonB='Hard Skills'
+        pathButtonB='/hard-skills'
+      />
+    </Styles.Main>
+  )
+}
+
+export default Contact;
