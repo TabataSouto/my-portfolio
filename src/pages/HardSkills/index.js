3e8a402fc0fd8e7865b73e452bfff279675f3093
Tabata <tabatac.souto@gmail.com>
2022-06-17 15:55:35 -0300

    feat: criação da rota e página /hard-skills e redirecionamento  no menu da página Home.

---
+++ src/pages/HardSkills/index.js
@@ -0,0 +1,7 @@
+function HardSkills() {
+  return (
+    <h1>Habilidades</h1>
+  )
+}
+
+export default HardSkills;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Home from '../pages/Home';
 import AboutMe from '../pages/AboutMe';
 import Projects from '../pages/Projects';
 import Contact from '../pages/Contact';
+import HardSkills from '../pages/HardSkills';
 
 function Routes() {
   return (
@@ -15,6 +16,7 @@ function Routes() {
         <Route path="/about" component={ AboutMe } />
         <Route path="/projects/" component={ Projects } />
         <Route path="/contact" component={ Contact } />
+        <Route path="/hard-skills" component={ HardSkills } />
       </Switch>
     </BrowserRouter>
   );
